463ae17e6547f53fa2d645fe767f8fe603b2dc63
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-30 16:24:26 +0100

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux'
 import { store } from './store.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>,
 )
