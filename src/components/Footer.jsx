748d6d228f9c7dc86b42dc0ee4b3ead1748a4df0
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-31 13:11:02 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,21 @@ import Subscribe from './Subscribe';
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-800 text-white py-8">
-        <div className="container mx-auto px-4">
-            <div className="flex flex-col md:flex-row justify-between items-start md:items-center">
-            <FooterLinksSection />
-            <FooterSocials />
-            </div>
+        <div className="relative mt-40">
+
+            <div className="absolute -top-24 left-1/2 transform -translate-x-1/2 z-10 w-full px-4 ">
             <Subscribe />
-            <p className="text-center mt-4 text-sm">© 2023 Shop.Co. All rights reserved.</p>
+            </div>
+            <footer className="bg-stone-300 text-black text-sm px-12 pt-32">
+                <div className="flex flex-wrap justify-between gap-8 px-6 py-10">
+                    <FooterLinksSection/>
+                </div>
+                <div className="flex flex-wrap justify-between items-start px-6 py-6 border-t border-stone-600 gap-8">
+                    <FooterSocials />
+                </div>
+            </footer>
         </div>
-        </footer>
     );
 };
+
+export default Footer;
