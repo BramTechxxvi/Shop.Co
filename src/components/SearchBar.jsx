d0f5ef323fc56c2e6efbd3bddab7cb5064bda9ab
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-31 11:17:42 +0100

    searchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,7 +12,16 @@ const SearchBar = ({ onSearch, className = "" }) => {
 
   return (
     <form className={`relative w-full ${className}`}>
-        <HiOutlineSearch className="absolute"/>
+        <HiOutlineSearch className="absolute top-1/2 left-4 -translate-y-1/2 text-gray-500 text-lg" />
+        <input
+        type="text"
+        value={query}
+        onChange={handleChange}
+        placeholder="Search for products..."
+        className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-green-800 shadow-sm text-sm sm:text-base"
+        />
     </form>
-  )
+  );
 }
+
+export default SearchBar;
