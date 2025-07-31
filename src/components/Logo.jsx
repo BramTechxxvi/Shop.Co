d68e9950f623638bda35c5d3222674c6363c1f7c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-31 11:04:56 +0100

    Logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
-export const Logo = () => {
+const Logo = () => {
   return (
-    <div>Logo</div>
-  )
+    <Link to="/" className="block">
+        <div className="font-extrabold tracking-tight text-xl sm:text-2xl md:text-3xl lg:text-4xl">
+            <span className="text-black">B</span>
+            <span className="text-amber-500">R</span>
+            <span className="text-black">A</span>
+            <span className="text-stone-700">M</span>
+            <span className="text-black">.</span>
+            <span className="text-stone-500">CO</span>
+        </div>
+    </Link>
+  );
 }
+
+export default Logo;
