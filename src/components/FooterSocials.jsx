178c4217a56bc80048a44d485f76581031026ec7
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-31 12:04:56 +0100

    update

---
+++ src/components/FooterSocials.jsx
@@ -0,0 +1,37 @@
+const paymentImages = ["Badge", "Badges", "Badge (1)", "Badge (3)"];
+
+export const FooterSocials = () => {
+  return (
+    <div className="w-full flex flex-col md:flex-row justify-between items-center gap-4 mt-6 px-4 ">
+        <p className="text-sm text-center md:text-left">
+            Kodak.co 2020202, All Rights Reserved
+        </p>
+
+        <div className="flex gap-2 flex-wrap justify-content md:justify-end cursor-pointer">
+            {paymentImages.map((img, index) => (
+                <img 
+                />
+            ))}
+        </div>
+
+
+    </div>
+  );
+}
+
+
+
+        {paymentImages.map((img, index) => (
+          <img
+            key={index}
+            src={`/${img}.png`}
+            alt={`${img} payment method`}
+            className="h-10 w-70 object-contain"
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FooterSocials;
