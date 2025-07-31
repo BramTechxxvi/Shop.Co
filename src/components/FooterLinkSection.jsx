6d95435b00d087bf008a0129304e8a832007b82c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-31 15:39:43 +0100

    pdate

---
+++ src/components/FooterLinkSection.jsx
@@ -0,0 +1,42 @@
+// import { Link } from "react-router-dom";
+// import { FaTwitter, FaInstagram, FaFacebookF, FaGithub, FaWhatsapp, FaSnapchat } from "react-icons/fa";
+
+
+const sections = [
+    {
+        title: "Company",
+        links: [
+            "About Us",
+            "About",
+            "Features",
+            "Careesrs",
+        ],
+    },
+    {
+        title: "Help",
+        links: [
+            "Customer support",
+            "Delivery Details",
+            "Terms & Conditions",
+            "Privacy Policy"
+        ]
+    },
+    {
+        title: "FAQ",
+        links: [
+            "Account",
+            "Manage Deliveries",
+            "Orders",
+            "Payment",
+        ]
+    },
+    {
+        title: "Resources",
+        links: [
+            "Free eBooks",
+            "Development Tutorial",
+            "How to - blog",
+            "Youtube Playlist",
+        ],
+    }
+];
