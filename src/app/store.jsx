aafd0065677cb95f6a96cce9539cbada732b440f
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-31 09:39:53 +0100

    store

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,6 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './api/CartSlice';
+import { productApi } from './api/ProductApi';
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    [productApi.reducerPath]: productApi.reducer,
+    cart: cartReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productApi.middleware),
 });
