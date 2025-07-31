39240729136c5ba771296d1cdd28b413940ce8cb
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-31 10:58:04 +0100

    create headrr

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import { useState} from 'react';
+import { IoMenu, IoClose } from 'react-icons/io5';
+import Logo from './Logo';
+
+export const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
