9cba7392f4ef95ddffe8fba31ef59a818eb19c84
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-31 11:21:19 +0100

    updatye

--- src/components/TopHeader.jsx
+++ src/components/TopHeader.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const TopHeader = () => {
-  return (
-    <div>TopHeader</div>
-  )
+    const [isVisible, setIsVisible] = useState(true);
+    if(!isVisible) return null;
+    
+    return (
+    <div className="bg-blue-500 text-white text-center p-2">
+
+    </div>
+  );
 }
+
+export default TopHeader;
