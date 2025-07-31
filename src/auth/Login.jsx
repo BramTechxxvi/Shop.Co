8108da05b83d5a8e1f3eab127dcddb3fd86b7622
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-31 10:55:16 +0100

    loader

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import Loader from '../components/Loader';
 
 export const Login = () => {
     const [phone, setPhone] = useState('');
