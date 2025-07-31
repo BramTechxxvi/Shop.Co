7f5f4757addc35afa813cfbed51d5bfb44a795bc
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-31 09:31:45 +0100

    cartSlice

--- src/api/CartSlice.jsx
+++ src/api/CartSlice.jsx
@@ -7,4 +7,48 @@ const cartSlice = createSlice({
   initialState: {
     cartItems: itemsInCart,
   },
-  reducers: {}
+  reducers: {
+    addToCart: (state, action) => {
+        const item = action.payload;
+        const existingItem = state.cartItems.find((itemInCart) => itemInCart.id === item.id);
+        if(existingItem) {
+            existingItem.quantity += 1;
+        } else {
+            state.cartItems.push({ ...item, quantity: 1 });
+
+        }
+    },
+
+    removeFromCart: (state, action) => {
+        state.cartItems = state.cartItems.filter((itemInCart) => itemInCart.id !== action.payload);
+    },
+
+    incrementQuantity: (state, action) => {
+        const item = state.cartItems.find((itemInCart) => itemInCart.id === action.payload);
+        if(item) {
+            item.quantity += 1;
+        }
+    },
+
+    decrementQuantity: (state, action) => {
+        const item = state.cartItems.find((itemInCart) => itemInCart.id === action.payload);
+        if(item && item.quantity > 1) {
+            item.quantity -= 1;
+        }
+    },
+
+    clearCart: (state) => {
+        state.cartItems = [];
+    },
+    },
+});
+
+export const { 
+    addToCart, 
+    removeFromCart, 
+    incrementQuantity, 
+    decrementQuantity, 
+    clearCart 
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
