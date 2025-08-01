6b7f63c5b82b009dbdc9310f6291fb74cea6a56b
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-01 13:14:27 +0100

    update

--- src/components/FooterLinkSection.jsx
+++ src/components/FooterLinkSection.jsx
@@ -1,7 +1,6 @@
 // import { Link } from "react-router-dom";
 // import { FaTwitter, FaInstagram, FaFacebookF, FaGithub, FaWhatsapp, FaSnapchat } from "react-icons/fa";
 
-
 const sections = [
     {
         title: "Company",
@@ -40,3 +39,22 @@ const sections = [
         ],
     }
 ];
+
+const FooterLinkSection = () => {
+  return (
+    <div>
+        <Link to="/" className="block">
+        <div className="font-extrabold tracking-tight text-xl sm:text-2xl md:text-3xl lg:text-4xl">
+            <span className="text-black">B</span>
+            <span className="text-amber-500">R</span>
+            <span className="text-black">A</span>
+            <span className="text-stone-700">M</span>
+            <span className="text-black">.</span>
+            <span className="text-stone-500">CO</span>
+        </div>
+        </Link>
+
+        <p></p>
+    </div>
+  )
+}
