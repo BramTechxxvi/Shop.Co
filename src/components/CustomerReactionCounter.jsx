b6d37f4fbe71d5d1877f14509eeabcc9f7fb2a28
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-01 16:08:50 +0100

    update

--- src/components/CustomerReactionCounter.jsx
+++ src/components/CustomerReactionCounter.jsx
@@ -1,4 +1,35 @@
 import CountUp from 'react-countup';
 
 const stats = [
-    { value: 270, suffix: "+", label: "Inter"}
+    { value: 270, suffix: "+", label: "International Brands" },
+    { value: 5000, suffix: "+", label: "High-Quality Products" },
+    { value: 100000, suffix: "+", label: "Happy Customers" },
+];
+
+
+const CustomerReactionCounter = () => {
+  return (
+    <section className="w-full py-10">
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-3 sm:grid-cols-3 gap-8 text-center sm:text-left">
+          {stats.map((stat, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center sm:items-start"
+            >
+              <p className="text-2xl sm:text-3xl md:text-2xl font-extrabold text-stone-800 hover:scale-105 transition duration-300 ease-in-out">
+                <CountUp end={stat.value} duration={4} separator="," />
+                {stat.suffix}
+              </p>
+              <span className="mt-2 text-sm sm:text-base text-gray-600">
+                {stat.label}
+              </span>
+            </div>
+          ))}
+        </div>
+      </div> 
+    </section>
+  );
+};
+
+export default CustomerReactionCounter;
