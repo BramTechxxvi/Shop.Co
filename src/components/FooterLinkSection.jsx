34f21ba2a3a5bb3d8fb0503eb2a20c6e447883a3
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-01 16:03:13 +0100

    footerlink

--- src/components/FooterLinkSection.jsx
+++ src/components/FooterLinkSection.jsx
@@ -41,7 +41,48 @@ const sections = [
 ];
 
 const FooterLinkSection = () => {
-  return ();
+  return (
+    <div>
+        <div>
+        <Link to="/" className="block">
+        <div className="font-extrabold tracking-tight text-xl sm:text-2xl md:text-3xl lg:text-4xl">
+            <span className="text-black">B</span>
+            <span className="text-amber-500">R</span>
+            <span className="text-black">A</span>
+            <span className="text-stone-700">M</span>
+            <span className="text-black">.</span>
+            <span className="text-stone-500">CO</span>
+        </div>
+        </Link>
+
+        <p classsName="w-48 py-5 hover:text-stone-900 transition-colors duration-200 text-stone-700">
+            We have clothes that suita your style and which you're proud to wear. From women to men.
+        </p>
+        <div className="flex gap-3">
+            <Link to="#"><FaTwitter className="text-xl" /></Link>
+            <Link to="#"><FaInstagram className="text-xl" /></Link>
+            <Link to="#"><FaFacebookF className="text-xl" /></Link>
+            <Link to="#"><FaGithub className="text-xl" /></Link>
+            <Link to="#"><FaWhatsapp className="text-xl" /></Link>
+            <Link to="#"><FaSnapchat className="text-xl" /></Link>
+        </div>
+    </div>
+    {sections.map((section, index) => (
+        <div key={index} className="w-full sm:w-auto">
+            <h3 className="text-lg font-semibold text-black mb-3">{section.title}</h3>
+            <ul className="space-y-1 etxt-gray-300">
+                {section.links.map((text, index) => (
+                    <li key={index}>
+                        <Link to="#" className="text-stone-700 hover:text-stone-900 hover:underline transition-colors duration-200">
+                            {text}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    ))}
+    </div>
+  );
 };
 
 export default FooterLinkSection;
