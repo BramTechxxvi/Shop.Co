db5422c48fa749f58828fd0abf8b9666d5a69cf2
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-05 04:08:18 +0100

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,11 +10,14 @@ const Footer = () => {
             <Subscribe />
             </div>
             <footer className="bg-stone-300 text-black text-sm px-12 pt-32">
-                <div className="flex flex-wrap justify-between gap-8 px-6 py-10">
-                    <FooterLinksSection/>
-                </div>
-                <div className="flex flex-wrap justify-between items-start px-6 py-6 border-t border-stone-600 gap-8">
-                    <FooterSocials />
+                <div>
+                    <div className="flex flex-wrap justify-between gap-8 px-6 py-10">
+                        <FooterLinksSection/>
+                    </div>
+
+                    <div className="flex flex-wrap justify-between items-start px-6 py-6 border-t border-stone-600 gap-8">
+                        <FooterSocials />
+                    </div>
                 </div>
             </footer>
         </div>
